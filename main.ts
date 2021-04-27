943c2967c75e21b4096a806e19598f02a76efb35
Smartfeld.ch <78407877+smartfeld-ch@users.noreply.github.com>
2021-04-27 10:07:30 +0200

    added tutorial.md

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P0, function () {
+    if (gesperrt == 0) {
+        gesperrt = 1
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
 })
+input.onPinPressed(TouchPin.P1, function () {
+    if (gesperrt == 0) {
+        gesperrt = 1
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+let gesperrt = 0
+basic.pause(randint(1, 5) * 1000)
+music.ringTone(494)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+music.stopAllSounds()
+gesperrt = 0
